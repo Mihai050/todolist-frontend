1215dd09b0defa58b5757b007b521e101d93b822
mihai050 <spyry4ever@gmail.com>
2023-08-16 15:57:08 +0300

    complete task page

--- src/routes/view-task/view-task.component.jsx
+++ src/routes/view-task/view-task.component.jsx
@@ -1,6 +1,6 @@
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from 'react'; 
-
+import './view-task.style.css';
 
 const ViewTask = () => {
     const { taskId } = useParams();
@@ -9,15 +9,38 @@ const ViewTask = () => {
     useEffect(() => {
         fetch(`/api/get-task/${taskId}`)
             .then((response) => response.json())
-            .then((data) => setTask(data));
+            .then((data) => {setTask(data)
+            console.log(data)});
+            
     }, [])
+
+    const estimatedTime = '2 hours';
+    
 
     
     return (
-      <div className="wrapper-body-page">
-        <h5>Title {task && task.title}</h5>
-        <h6>Task type: {task && task.taskType}</h6>
-        <h6>Task details: {task && task.description}</h6>
+      <div className="container">
+        <div className="header">
+          <h3>{task && task.title}</h3>
+          <h5>Task type: {task && task.taskType}</h5>
+        </div>
+
+        <h5> {task && task.description}</h5>
+        <div className="">
+          <h5>Task created at: {task && task.createdAt}</h5>
+          <h5>Task deadline: {task && task.deadline}</h5>
+          <h5>Estimated time {task && task.deadline}</h5>
+        </div>
+
+        <h5>Task status: {task && task.status}</h5>
+
+        
+          <h5>
+            Time spent on task:
+            <input type="text" placeholder="Time spent" />
+          </h5>
+        
+        <p className="submit-button">Complete</p>
       </div>
     );
 };
