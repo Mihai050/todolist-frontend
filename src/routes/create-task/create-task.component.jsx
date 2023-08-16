1a1c1e244aaae940c39aa367869749900f6c5f86
mihai050 <spyry4ever@gmail.com>
2023-08-16 15:17:11 +0300

    create task page

--- src/routes/create-task/create-task.component.jsx
+++ src/routes/create-task/create-task.component.jsx
@@ -7,14 +7,41 @@ const CreateTask = () => {
   const currentDate = `${currentYear}-${currentMonth}-${currentDay}`;
   console.log(currentDate);
 
-  return <div className="wrapper-body-page">
-    <div className="upper-info"></div>
-    <div className="error-info"></div>
-    <div className="form-section">
-      <input type='date' min={currentDate} label='get finish day'/>
+  return (
+    <div className="form-container">
+      <div className="upper-info"></div>
+      <div className="error-info"></div>
+      <div className="form-section">
+        <p className="form-title">Add new task</p>
+        <p className='warning'>This is a warning</p>
+        <div class="form-element">
+          <p>Title:</p>
+          <input type="text" className="" placeholder="Title" />
+        </div>
+        <div class="form-element">
+          <p>Description:</p>
+          <textarea />
+        </div>
+
+        <div class="form-element">
+          <p>End date:</p>
+          <input type="date" min={currentDate} label="get finish day" />
+        </div>
+        <div class="form-element">
+          <p>Estimated duration:</p>
+          <input type="text" />
+        </div>
+        <div class="form-element">
+          <p>Type:</p>
+          <select name="Type" id="type">
+            <option value="Hobby">Hobby</option>
+            <option value="Not hobbi">not hobbi</option>
+          </select>
+        </div>
+      </div>
+      <div className="button-wrapper"></div>
     </div>
-    <div className="button-wrapper"></div>
-  </div>;
+  );
 };
 
 export default CreateTask;
